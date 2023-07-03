554b7e26721342686961370e93984b9621483da1
Matheus Schittini <matheusschittinimc@gmail.com>
2023-07-03 13:58:47 -0300

    Task 5 done

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return Object.keys(user).length > 0 ? (
     <div style={{padding: "40px"}}>
       <h1>Name: {user.results[0].name.first}</h1>
+      <img src={user.results[0].picture.large} alt=""></img>
     </div>
   ) : (
     <h1>Data pending...</h1>
